433da8bd9e6f5c988470c0298e922ce5a0ef7509
Noah Luddy <noahluddy@wustl.edu>
2017-01-24 21:53:17 -0600

    final

--- index.php
+++ index.php
@@ -1,31 +1,29 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title> omgplzwurk </title>
-	</head>
-	
-	<body>
-		<p> This is a test. </p>
-	</body>
-	<?php
-	// Include the AWS SDK using the Composer autoloader.
-	require 'vendor/autoload.php';
+  <head>
+    <title>Geolocation</title>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
 
-	$s3 = Aws\S3\S3Client::factory();
-	$bucket = getenv('S3_BUCKET');
-	$keyname = 'test.txt';
-	try {
-		// Upload data.
-		$result = $s3->putObject(array(
-			'Bucket' => $bucket,
-			'Key'    => $keyname,
-			'Body'   => 'Hello, world!',
-			'ACL'    => 'public-read'
-		));
-		// Print the URL to the object.
-		echo $result['ObjectURL'] . "\n";
-	} catch (S3Exception $e) {
-		echo $e->getMessage() . "\n";
-	}
-	?>
+      <!--links css to html-->
+      <link rel="stylesheet" type="text/css" href="style.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+      <script src=javascript.js></script>
+
+  </head>
+  <body>
+    <div id="map"></div>
+    <h2 id="distance"></h2>
+    <h2 id="lat_long"></h2>
+    <p id="timer">Click on the button below to start the timer!</p>
+    <button onClick="myTimer = setInterval(myCounter, 1000)">Start timer!</button>
+    <button onClick="clearInterval(myTimer); write_file();">Stop timer!</button>
+    <h3 id="stuff"></h3>
+    <!-- <form>
+  <input type="button" value="Submit" onclick="clickEvent();" />
+</form> -->
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAf0Nha2Aj10tTGo39umyxCaCgw5BOkIyI&callback=initMap">
+    </script>
+  </body>
 </html>
--- javascript.js
+++ javascript.js
@@ -1,74 +1,76 @@
 // Note: This example requires that you consent to location sharing when
-      // prompted by your browser. If you see the error "The Geolocation service
-      // failed.", it means you probably did not give permission for the browser to
-      // locate you.
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+var d;
 
-      function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 18
-        });
-        var infoWindow = new google.maps.InfoWindow({map: map});
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 18
+  });
+  var infoWindow = new google.maps.InfoWindow({map: map});
 
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-            // document.getElementById("distance1").innerHTML = pos.lat;
-            // document.getElementById("distance2").innerHTML = pos.lng;
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      // document.getElementById("distance1").innerHTML = pos.lat;
+      // document.getElementById("distance2").innerHTML = pos.lng;
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            map.setCenter(pos);
-  var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(38.6360-pos.lat);  // deg2rad below
-  var dLon = deg2rad(-90.2624-pos.lng);
-  var a =
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(pos.lat)) * Math.cos(deg2rad(38.6360)) *
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ;
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-  var d = R * c; // Distance in km
-  document.getElementById("distance").innerHTML = "You are " + d + " km away from Central West End Station.";
-  document.getElementById("lat_long").innerHTML = "The exact coordinates of Central West End Station are: 38.6360&deg;N and -90.2624&deg;W.";
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+      var R = 6371; // Radius of the earth in km
+      var dLat = deg2rad(38.6360-pos.lat);  // deg2rad below
+      var dLon = deg2rad(-90.2624-pos.lng);
+      var a =
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(pos.lat)) * Math.cos(deg2rad(38.6360)) *
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ;
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+      d = R * c; // Distance in km
+      document.getElementById("distance").innerHTML = "You are " + d + " km away from Central West End Station.";
+      document.getElementById("lat_long").innerHTML = "The exact coordinates of Central West End Station are: 38.6360&deg;N and -90.2624&deg;W.";
 
-function deg2rad(deg) {
-  return deg * (Math.PI/180)
-}
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
+      function deg2rad(deg) {
+        return deg * (Math.PI/180)
       }
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-      }
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+      'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
+    }
+  }
 
-      var sec = 0;
-      function myCounter() {
-          document.getElementById("timer").innerHTML = "You've waited " + ++sec + " seconds.";
-      }
+  var sec = 0;
+  function myCounter() {
+    document.getElementById("timer").innerHTML = "You've waited " + ++sec + " seconds.";
+  }
 
-      function write_file() {
-        var data {
-          seconds: sec,
-          distance: d,
-          latitude: 38.6360,
-          longitude: -90.2624
-        }
-        
-      }
-// function clickEvent (){
-//     alert("Hey!");
-// }
+  function write_file() {
+    var data = {
+      seconds: sec,
+      distance: d,
+      latitude: 38.6360,
+      longitude: -90.2624
+    }
+    console.log(data);
+    // var fso = new ActiveXObject("Scripting.FileSystemObject");
+    // var fh = fso.OpenTextFile("data.txt", 8, false, 0);
+    // fh.WriteLine("Test data.");
+    // fh.Close();
+  }
