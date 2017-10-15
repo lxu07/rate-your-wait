e827a7b4c9ef4c8e878887ad07c780e3c4b9b393
lxu07 <lilyxu24k@gmail.com>
2017-10-14 23:17:46 -0500

    Add files via upload
    Tester

--- index.php
+++ index.php
@@ -1,32 +1,46 @@
 <!DOCTYPE html>
+
 <html>
-  <head>
-    <img src="logo.png"/>
-    <title>Geolocation</title>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
 
-      <!--links css to html-->
-      <link rel="stylesheet" type="text/css" href="style.css">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-      <script src=javascript.js></script>
-      <link rel="stylesheet" type="text/css" href="ryw_style.css" media="screen" />
+<head>
+<title>Synesthesia</title>
+<link rel="stylesheet" type="text/css" href="synstyle.css">
+<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'>
+</head>
 
-  </head>
-  <body>
-    <div id="map"></div>
-    <h2 id="distance"></h2>
-    <h2 id="lat_long"></h2>
-    <p id="timer">Click on the button below to start the timer!</p>
-    <button onClick="myTimer = setInterval(myCounter, 1000)">Start timer!</button>
-    <button onClick="clearInterval(myTimer); write_file();">Stop timer!</button>
-    <h3 id="stuff"></h3>
-    <!-- <form>
-  <input type="button" value="Submit" onclick="clickEvent();" />
-</form> -->
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAf0Nha2Aj10tTGo39umyxCaCgw5BOkIyI&callback=initMap">
-    </script>
-    <img src="bus.png"/>
-  </body>
+<body>
+<div id = "header">
+	<a href = "https://google.com"><img src="logosilence.png" alt="Synethesia" width="330" height="100"></a>
+</div>
+
+<form action="index.html" method="get">
+	<h3>Enter the start time, end time, and interval.</h3>
+	<div id="questions">
+		Start Day (MM/DD/YYYY): <input type="date" name="startDayText" min="1970-01-01" required="true"><br>
+		Start Time (HH:MM AM/PM): <input type="time" name="startTimeText" required="true"><br>
+		End Day (MM/DD/YYYY): <input type="date" name="endDayText" min="1970-01-01" required="true"><br>
+		End Time (HH:MM AM/PM): <input type="time" name="endTimeText" required="true"><br>
+		Interval (Hrs): <input type="number" name="intervalText" min="1" required="true"><br>
+	</div>
+	<h3>Choose the type of data to be visualized.</h3>
+	<div id="checkboxes">
+		<input type="checkbox" name="option1"> Temperature<br>
+		<input type="checkbox" name="option2"> Pedestrian Flow<br>
+		<input type="checkbox" name="option3"> Pressure<br>
+		<input type="checkbox" name="option4"> Humidity<br>
+		<input type="checkbox" name="option5"> Traffic<br>
+		<input type="checkbox" name="option5"> Audio Level<br><br>
+	</div>
+
+	<input type="submit" value="Submit" onclick="">
+	<input type="reset">
+</form>
+	<div><a href = "map.html"><button>Let's map it!</button></a></div>
+
+</body>
+
+<script>
+</script>
+
+
 </html>
